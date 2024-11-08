5813af1ea20e7d40e9f44a7ce3caf3e21beac013
Abdel780 <abdelkarim27200@gmail.com>
2024-11-08 14:27:02 +0100

    push de style de la page

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 let count = 0;
 
-document.getElementById("button-clicker").addEventListener("click",() => {
+document.getElementById('button-clicker')
+        .addEventListener('click', () =>{
     count++;
-    document.getElementById("counter").innerHTML = count;
+    document.getElementById('counter').innerHTML = count;
+    if(count >= 0) {
+        document.getElementById('counter').style.backgroundColor = 'green';
+        document.getElementById('counter').style.color = 'white';
+    } else {
+        document.getElementById('counter').style.backgroundColor = 'orange';
+        document.getElementById('counter').style.color = 'blue';
+    }
+});
+
+document.getElementById('button').addEventListener('click', () => {
+    count--;
+    document.getElementById('counter').innerHTML = count;
+if(count < 0) {
+    document.getElementById('counter').style.backgroundColor = 'orange';
+    document.getElementById('counter').style.color = 'blue';
+} else {
+    document.getElementById('counter').style.backgroundColor = 'grey';
+    document.getElementById('counter').style.color = 'white';
+}
+})
+
+document.getElementById('reinitialiser').addEventListener('click', () => {
+    count = 0;
+    document.getElementById('counter').innerHTML = count;
+    document.getElementById('counter').style.backgroundColor = 'green';
 })
